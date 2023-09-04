ad1bf33d77898049e05d9c5d63344e5e800729a8
Josh <jw48152@gmail.com>
2023-09-04 02:33:29 -0400

    queue playground

--- udemy-queue.c
+++ udemy-queue.c
@@ -2,9 +2,30 @@
 #include <stdint.h>
 #include <stdlib.h>
 
+typedef struct QNode
+{
+  int key;
+  struct QNode *next;
+} qnode_t;
+
+typedef struct Queue
+{
+  struct qnode_t *front;
+  struct qnode_t *rear;
+} queue_t;
+
+qnode_t *NewNode(int key_)
+{
+  qnode_t *temp = (qnode_t *)malloc(sizeof(qnode_t));
+  temp->key = key_;
+  temp->next = NULL;
+  return temp;
+}
+
 int main(void)
 {
   // todo
-
+  qnode_t *qnode_test = NewNode(6);
+  printf("%d\n", qnode_test->key);
   return 0;
 }
