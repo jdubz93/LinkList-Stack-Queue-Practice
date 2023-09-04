3cae0bba3ffa34bb139e022c7d8bab066b2074c5
Josh <jw48152@gmail.com>
2023-09-04 02:35:24 -0400

    queue playground

--- udemy-queue.c
+++ udemy-queue.c
@@ -22,10 +22,20 @@ qnode_t *NewNode(int key_)
   return temp;
 }
 
+queue_t *CreateQueue(void)
+{
+  queue_t *que = malloc(sizeof(queue_t));
+  que->rear = NULL;
+  que->front = que->rear;
+  return que;
+}
+
 int main(void)
 {
   // todo
   qnode_t *qnode_test = NewNode(6);
   printf("%d\n", qnode_test->key);
+  queue_t *my_queue = CreateQueue();
+
   return 0;
 }
